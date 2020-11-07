c7a37542911dcc6ea38416a2eac33cb99be4ee27
BY2011 <96490111+BY2011@users.noreply.github.com>
2020-11-07 12:48:08 +0000

    added images to readme

--- contracts/mpc1-multisig/src/state.rs
+++ contracts/mpc1-multisig/src/state.rs
@@ -3,4 +3,7 @@ use mpc1_multisig_base::state::MPC1MultisigContractState;
 
 #[state]
 #[derive(PartialEq, Eq, Debug)]
-pub struct Cont
+pub struct ContractState {
+    pub mpc1_multisig: MPC1MultisigContractState,
+    pub version: ContractVersionBase,
+}
