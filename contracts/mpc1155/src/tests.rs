bd36429b44795acc0ad4df584977f516c02d576e
BY2011 <96490111+BY2011@users.noreply.github.com>
2020-11-28 13:26:46 +0000

    Update outdated dependencies

--- contracts/mpc1155/src/tests.rs
+++ contracts/mpc1155/src/tests.rs
@@ -6,4 +6,10 @@ use pbc_contract_common::{
     address::{Address, AddressType, Shortname},
     events::EventGroup,
 };
-use utils::events::IntoShort
+use utils::events::IntoShortnameRPCEvent;
+
+fn mock_address(le: u8) -> Address {
+    Address {
+        address_type: AddressType::Account,
+        identifier: [
+            le, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8,
