12549e6b36cf6eb4ab3de74e9e61204a5a64a9c9
BY2011 <96490111+BY2011@users.noreply.github.com>
2020-11-28 05:37:00 +0000

    smh

--- contracts/mpc1155/src/tests.rs
+++ contracts/mpc1155/src/tests.rs
@@ -1,3 +1,9 @@
 use mpc1155_base::msg::{
     ApproveForAllMsg, BatchBurnMsg, BatchMintMsg, BatchTransferFromMsg, BurnMsg, CheckBalancesMsg,
-    MintMsg, RevokeForAllMsg, SetUriMsg, TokenMintInfoMsg, TokenTransferInf
+    MintMsg, RevokeForAllMsg, SetUriMsg, TokenMintInfoMsg, TokenTransferInfoMsg, TransferFromMsg,
+};
+use pbc_contract_common::{
+    address::{Address, AddressType, Shortname},
+    events::EventGroup,
+};
+use utils::events::IntoShort
