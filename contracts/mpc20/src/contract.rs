9c35688cf181795754968430a6523e23637e9743
BY2011 <96490111+BY2011@users.noreply.github.com>
2021-04-04 10:10:04 +0000

    build: upgrade dependencies

--- contracts/mpc20/src/contract.rs
+++ contracts/mpc20/src/contract.rs
@@ -1,1 +1,7 @@
-use crate::state::TokenSta
+use crate::state::TokenState;
+
+use contract_version_base::state::ContractVersionBase;
+use pbc_contract_common::{address::Address, context::ContractContext, events::EventGroup};
+
+use mpc20_base::{
+    ac
