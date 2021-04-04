9fed032bdaddf29907738b55d0f86f4d270ca6f6
BY2011 <96490111+BY2011@users.noreply.github.com>
2021-04-04 18:35:38 +0000

    add exception handler

--- contracts/mpc20/src/contract.rs
+++ contracts/mpc20/src/contract.rs
@@ -6,4 +6,8 @@ use pbc_contract_common::{address::Address, context::ContractContext, events::Ev
 use mpc20_base::{
     actions::{
         execute_approve, execute_burn, execute_burn_from, execute_decrease_allowance,
-        execute_increase_allowance, execute_init,
+        execute_increase_allowance, execute_init, execute_mint, execute_transfer,
+        execute_transfer_from,
+    },
+    msg::{
+        ApproveMsg, BurnFromMsg, BurnMsg, DecreaseAllowanceMsg, IncreaseAllowanceMsg, Mi
