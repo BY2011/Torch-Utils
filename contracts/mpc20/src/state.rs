7ab6f80cd48a270875fc1493254a3dd4762bcc30
BY2011 <96490111+BY2011@users.noreply.github.com>
2021-05-23 19:30:32 +0000

    Fix lint errors

--- contracts/mpc20/src/state.rs
+++ contracts/mpc20/src/state.rs
@@ -1,1 +1,7 @@
-use contrac
+use contract_version_base::state::ContractVersionBase;
+use mpc20_base::state::MPC20ContractState;
+
+#[state]
+#[derive(PartialEq, Eq, Debug)]
+pub struct TokenState {
+    pub mpc20: MPC2
