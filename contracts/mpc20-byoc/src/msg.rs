7238c244e07a7abc0c8432e68cec630b24c8392b
BY2011 <96490111+BY2011@users.noreply.github.com>
2021-06-03 21:47:57 +0000

    Updated description

--- contracts/mpc20-byoc/src/msg.rs
+++ contracts/mpc20-byoc/src/msg.rs
@@ -1,4 +1,9 @@
 use create_type_spec_derive::CreateTypeSpec;
 use mpc20_base::msg::Mpc20InitMsg;
 use pbc_contract_common::address::{Address, Shortname};
-use 
+use read_write_rpc_derive::ReadWriteRPC;
+
+use rpc_msg_derive::IntoShortnameRPCEvent;
+use utils::events::IntoShortnameRPCEvent;
+
+#[derive(ReadWriteRPC, CreateTypeSpec, Clone, P
