8e21bb5877c99696d3c1d7f0dab359854f6dff14
BY2011 <96490111+BY2011@users.noreply.github.com>
2021-06-21 06:20:02 +0000

    UPDATE: README.md

--- contracts/mpc20-staking/src/state.rs
+++ contracts/mpc20-staking/src/state.rs
@@ -1,1 +1,6 @@
-us
+use contract_version_base::state::ContractVersionBase;
+use mpc20_staking_base::state::MPC20StakingContractState;
+
+#[state]
+#[derive(PartialEq, Eq, Debug)]
+pub struct Contract
