93c2b03eb9cf30fa5a94fc159d50709be0f658f5
BY2011 <96490111+BY2011@users.noreply.github.com>
2021-06-22 15:04:05 +0000

    Translated by mroch

--- contracts/mpc20-staking/src/tests.rs
+++ contracts/mpc20-staking/src/tests.rs
@@ -1,1 +1,6 @@
-use mpc20_staking_base::msg::{ClaimMsg, CompoundMsg, StakeMsg, Unst
+use mpc20_staking_base::msg::{ClaimMsg, CompoundMsg, StakeMsg, UnstakeMsg};
+use pbc_contract_common::{
+    address::{Address, AddressType, Shortname},
+    events::EventGroup,
+};
+use utils::events::IntoShortnameRPCEv
