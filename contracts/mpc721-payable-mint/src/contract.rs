03321288d2e25cc7ee766fda3a2a8b1cd0d1df2e
BY2011 <96490111+BY2011@users.noreply.github.com>
2021-08-18 16:40:28 +0000

    hwmon: (adt7470) Fix writes to temperature limit registers

--- contracts/mpc721-payable-mint/src/contract.rs
+++ contracts/mpc721-payable-mint/src/contract.rs
@@ -74,3 +74,171 @@ pub fn transfer_from(
     let events = execute_transfer_from(
         &ctx,
         &mut state.mpc721,
+        &TransferFromMsg { from, to, token_id },
+    );
+
+    (state, events)
+}
+
+#[action(shortname = 0x05)]
+pub fn approve(
+    ctx: ContractContext,
+    state: ContractState,
+    spender: Address,
+    token_id: u128,
+) -> (ContractState, Vec<EventGroup>) {
+    let mut state = state;
+    let events = execute_approve(&ctx, &mut state.mpc721, &ApproveMsg { spender, token_id });
+
+    (state, events)
+}
+
+#[action(shortname = 0x07)]
+pub fn set_base_uri(
+    ctx: ContractContext,
+    state: ContractState,
+    new_base_uri: String,
+) -> (ContractState, Vec<EventGroup>) {
+    let mut state = state;
+    let events = execute_set_base_uri(&ctx, &mut state.mpc721, &SetBaseUriMsg { new_base_uri });
+
+    (state, events)
+}
+
+#[action(shortname = 0x09)]
+pub fn mint(
+    ctx: ContractContext,
+    state: ContractState,
+    token_id: u128,
+    to: Address,
+    token_uri: Option<String>,
+) -> (ContractState, Vec<EventGroup>) {
+    let mut payout_transfer_events = EventGroup::builder();
+
+    MPC20TransferFromMsg {
+        from: to,
+        to: ctx.contract_address,
+        amount: state.payable_mint_info.amount,
+    }
+    .as_interaction(&mut payout_transfer_events, &state.payable_mint_info.token);
+
+    build_msg_callback(
+        &mut payout_transfer_events,
+        0x30,
+        &MintMsg {
+            token_id,
+            to,
+            token_uri,
+        },
+    );
+
+    (state, vec![payout_transfer_events.build()])
+}
+
+#[callback(shortname = 0x30)]
+pub fn on_mint_callback(
+    ctx: ContractContext,
+    callback_ctx: CallbackContext,
+    mut state: ContractState,
+    msg: MintMsg,
+) -> (ContractState, Vec<EventGroup>) {
+    assert_callback_success(&callback_ctx);
+    let _ = execute_mint(&ctx, &mut state.mpc721, &msg);
+
+    let mut payout_transfer_events = EventGroup::builder();
+
+    MPC20TransferMsg {
+        to: state.mpc721.owner.unwrap(),
+        amount: state.payable_mint_info.amount,
+    }
+    .as_interaction(&mut payout_transfer_events, &state.payable_mint_info.token);
+
+    (state, vec![payout_transfer_events.build()])
+}
+
+#[action(shortname = 0x11)]
+pub fn approve_for_all(
+    ctx: ContractContext,
+    state: ContractState,
+    operator: Address,
+) -> (ContractState, Vec<EventGroup>) {
+    let mut state = state;
+    let events = execute_approve_for_all(&ctx, &mut state.mpc721, &ApproveForAllMsg { operator });
+
+    (state, events)
+}
+
+#[action(shortname = 0x13)]
+pub fn revoke(
+    ctx: ContractContext,
+    state: ContractState,
+    spender: Address,
+    token_id: u128,
+) -> (ContractState, Vec<EventGroup>) {
+    let mut state = state;
+    let events = execute_revoke(&ctx, &mut state.mpc721, &RevokeMsg { spender, token_id });
+
+    (state, events)
+}
+
+#[action(shortname = 0x15)]
+pub fn revoke_for_all(
+    ctx: ContractContext,
+    state: ContractState,
+    operator: Address,
+) -> (ContractState, Vec<EventGroup>) {
+    let mut state = state;
+    let events = execute_revoke_for_all(&ctx, &mut state.mpc721, &RevokeForAllMsg { operator });
+
+    (state, events)
+}
+
+#[action(shortname = 0x17)]
+pub fn burn(
+    ctx: ContractContext,
+    state: ContractState,
+    token_id: u128,
+) -> (ContractState, Vec<EventGroup>) {
+    let mut state = state;
+    let events = execute_burn(&ctx, &mut state.mpc721, &BurnMsg { token_id });
+
+    (state, events)
+}
+
+#[action(shortname = 0x18)]
+pub fn check_ownership(
+    ctx: ContractContext,
+    state: ContractState,
+    owner: Address,
+    token_id: u128,
+) -> (ContractState, Vec<EventGroup>) {
+    let mut state = state;
+    let events =
+        execute_ownership_check(&ctx, &mut state.mpc721, &CheckOwnerMsg { owner, token_id });
+    (state, events)
+}
+
+#[action(shortname = 0x19)]
+pub fn update_minter(
+    ctx: ContractContext,
+    state: ContractState,
+    new_minter: Address,
+) -> (ContractState, Vec<EventGroup>) {
+    let mut state = state;
+    let events = execute_update_minter(&ctx, &mut state.mpc721, UpdateMinterMsg { new_minter });
+    (state, events)
+}
+
+#[action(shortname = 0x20)]
+pub fn multi_mint(
+    ctx: ContractContext,
+    state: ContractState,
+    mints: Vec<MintMsg>,
+) -> (ContractState, Vec<EventGroup>) {
+    assert!(!mints.is_empty(), "At least one mint msg should be passed");
+
+    let receiver = mints[0].to;
+    assert!(
+        !mints.iter().any(|mint| mint.to != receiver),
+        "Only one receiver supported in payable multi-mint"
+    );
