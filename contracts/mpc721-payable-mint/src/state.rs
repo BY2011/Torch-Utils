6e766f2854b0c8e0cc56eccb788a08ce2a60abd1
BY2011 <96490111+BY2011@users.noreply.github.com>
2021-08-21 11:13:26 +0000

    just test

--- contracts/mpc721-payable-mint/src/state.rs
+++ contracts/mpc721-payable-mint/src/state.rs
@@ -1,3 +1,9 @@
 use contract_version_base::state::ContractVersionBase;
 use create_type_spec_derive::CreateTypeSpec;
-use mpc721_base::state::MPC721ContractSt
+use mpc721_base::state::MPC721ContractState;
+use pbc_contract_common::address::Address;
+use read_write_rpc_derive::ReadWriteRPC;
+use read_write_state_derive::ReadWriteState;
+
+#[state]
+#[derive(PartialEq, Eq
