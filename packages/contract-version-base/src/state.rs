1cbc278328dd5fae98870934b1f3ce5df33aeabb
BY2011 <96490111+BY2011@users.noreply.github.com>
2021-09-06 21:40:46 +0000

    Simplify build

--- packages/contract-version-base/src/state.rs
+++ packages/contract-version-base/src/state.rs
@@ -2,4 +2,6 @@ use create_type_spec_derive::CreateTypeSpec;
 use read_write_state_derive::ReadWriteState;
 
 /// ## Description
-/// This structure 
+/// This structure describes contract version base state
+#[derive(ReadWriteState, CreateTypeSpec, Clone, PartialEq, Eq, Debug, Default)]
+pub struct ContractVersionBa
