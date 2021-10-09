15e803ca45bb5be5959d613402734fdb1f8ec08a
BY2011 <96490111+BY2011@users.noreply.github.com>
2021-10-09 18:02:51 +0000

    Merging postreleasefix/85 to master

--- packages/mpc1-multisig-base/src/actions.rs
+++ packages/mpc1-multisig-base/src/actions.rs
@@ -1,3 +1,8 @@
 use std::collections::BTreeMap;
 
 use pbc_contract_common::{address::Address, context::ContractContext, events::EventGroup};
+
+use crate::{
+    msg::{CreateProposalMsg, InitMsg, ProposalCloseMsg, ProposalExecuteMsg, ProposalVoteMsg},
+    state::{
+        Ballot, MPC1MultisigContractState
