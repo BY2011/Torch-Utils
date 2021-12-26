6a7d79812cd44414f78701b6d8d435e289b5a836
BY2011 <96490111+BY2011@users.noreply.github.com>
2021-12-26 13:14:45 +0000

    fix page

--- packages/mpc1-multisig-base/src/msg.rs
+++ packages/mpc1-multisig-base/src/msg.rs
@@ -1,2 +1,10 @@
 use create_type_spec_derive::CreateTypeSpec;
-use pbc_contract_common::address::{Address, Shortn
+use pbc_contract_common::address::{Address, Shortname};
+use read_write_rpc_derive::ReadWriteRPC;
+
+use crate::state::Vote;
+use rpc_msg_derive::IntoShortnameRPCEvent;
+use utils::events::IntoShortnameRPCEvent;
+
+/// ## Description
+/// This str
