bc74db9e536c9c8242516b2a394c1b76b9e300be
BY2011 <96490111+BY2011@users.noreply.github.com>
2022-02-12 21:41:20 +0000

    removed logger

--- packages/mpc1155-base/src/actions.rs
+++ packages/mpc1155-base/src/actions.rs
@@ -137,3 +137,181 @@ pub fn execute_batch_mint(
 /// Returns [`(MPC1155ContractState, Vec<EventGroup>)`] if operation was successful,
 /// otherwise panics with error message defined in [`ContractError`]
 /// ## Params
+/// * **ctx** is an object of type [`ContractContext`]
+///
+/// * **state** is an object of type [`MPC1155ContractState`]
+///
+/// * **msg** is an object of type [`TransferFromMsg`]
+pub fn execute_transfer_from(
+    ctx: &ContractContext,
+    state: &mut MPC1155ContractState,
+    msg: &TransferFromMsg,
+) -> Vec<EventGroup> {
+    assert!(
+        state.is_token_owner_or_operator(&msg.from, &ctx.sender),
+        "{}",
+        ContractError::Unauthorized,
+    );
+
+    state.transfer(
+        Some(&msg.from),
+        Some(&msg.to),
+        msg.token_info.token_id,
+        msg.token_info.amount,
+    );
+
+    vec![]
+}
+
+/// ## Description
+/// Only with approval extension. Batch transfer token from owner to spender.
+/// Returns [`(MPC1155ContractState, Vec<EventGroup>)`] if operation was successful,
+/// otherwise panics with error message defined in [`ContractError`]
+/// ## Params
+/// * **ctx** is an object of type [`ContractContext`]
+///
+/// * **state** is an object of type [`MPC1155ContractState`]
+///
+/// * **msg** is an object of type [`BatchTransferFromMsg`]
+pub fn execute_batch_transfer_from(
+    ctx: &ContractContext,
+    state: &mut MPC1155ContractState,
+    msg: &BatchTransferFromMsg,
+) -> Vec<EventGroup> {
+    assert!(
+        state.is_token_owner_or_operator(&msg.from, &ctx.sender),
+        "{}",
+        ContractError::Unauthorized,
+    );
+
+    for token_info in msg.token_infos.iter() {
+        state.transfer(
+            Some(&msg.from),
+            Some(&msg.to),
+            token_info.token_id,
+            token_info.amount,
+        );
+    }
+
+    vec![]
+}
+
+/// ## Description
+/// Destroy your token forever.
+/// Returns [`(MPC1155ContractState, Vec<EventGroup>)`] if operation was successful,
+/// otherwise panics with error message defined in [`ContractError`]
+/// ## Params
+/// * **ctx** is an object of type [`ContractContext`]
+///
+/// * **state** is an object of type [`MPC1155ContractState`]
+///
+/// * **msg** is an object of type [`BurnMsg`]
+pub fn execute_burn(
+    ctx: &ContractContext,
+    state: &mut MPC1155ContractState,
+    msg: &BurnMsg,
+) -> Vec<EventGroup> {
+    assert!(
+        state.is_token_owner_or_operator(&msg.from, &ctx.sender),
+        "{}",
+        ContractError::Unauthorized,
+    );
+
+    state.transfer(
+        Some(&msg.from),
+        None,
+        msg.token_info.token_id,
+        msg.token_info.amount,
+    );
+
+    vec![]
+}
+
+/// ## Description
+/// Batch destroy your token forever.
+/// Returns [`(MPC1155ContractState, Vec<EventGroup>)`] if operation was successful,
+/// otherwise panics with error message defined in [`ContractError`]
+/// ## Params
+/// * **ctx** is an object of type [`ContractContext`]
+///
+/// * **state** is an object of type [`MPC1155ContractState`]
+///
+/// * **msg** is an object of type [`BatchBurnMsg`]
+pub fn execute_batch_burn(
+    ctx: &ContractContext,
+    state: &mut MPC1155ContractState,
+    msg: &BatchBurnMsg,
+) -> Vec<EventGroup> {
+    assert!(
+        state.is_token_owner_or_operator(&msg.from, &ctx.sender),
+        "{}",
+        ContractError::Unauthorized,
+    );
+
+    for token_info in msg.token_infos.iter() {
+        state.transfer(
+            Some(&msg.from),
+            None,
+            token_info.token_id,
+            token_info.amount,
+        );
+    }
+
+    vec![]
+}
+
+/// ## Description
+/// Allows operator to transfer any owner tokens from his account.
+/// Returns [`(MPC1155ContractState, Vec<EventGroup>)`] if operation was successful,
+/// otherwise panics with error message defined in [`ContractError`]
+/// ## Params
+/// * **ctx** is an object of type [`ContractContext`]
+///
+/// * **state** is an object of type [`MPC1155ContractState`]
+///
+/// * **msg** is an object of type [`ApproveForAllMsg`]
+pub fn execute_approve_for_all(
+    ctx: &ContractContext,
+    state: &mut MPC1155ContractState,
+    msg: &ApproveForAllMsg,
+) -> Vec<EventGroup> {
+    state.add_operator(&ctx.sender, &msg.operator);
+    vec![]
+}
+
+/// ## Description
+/// Remove operator.
+/// Returns [`(MPC1155ContractState, Vec<EventGroup>)`] if operation was successful,
+/// otherwise panics with error message defined in [`ContractError`]
+/// ## Params
+/// * **ctx** is an object of type [`ContractContext`]
+///
+/// * **state** is an object of type [`MPC1155ContractState`]
+///
+/// * **msg** is an object of type [`RevokeForAllMsg`]
+pub fn execute_revoke_for_all(
+    ctx: &ContractContext,
+    state: &mut MPC1155ContractState,
+    msg: &RevokeForAllMsg,
+) -> Vec<EventGroup> {
+    state.remove_operator(&ctx.sender, &msg.operator);
+    vec![]
+}
+/// ## Description
+/// Assert that a user owns a given amount of nfts of a different id.
+/// Returns [ Vec<EventGroup>)`] if operation was successful,
+/// otherwise panics with error message defined in [`InadequateBalance`]
+/// ## Params
+/// * **ctx** is an object of type [`ContractContext`]
+///
+/// * **state** is an object of type [`MPC1155ContractState`]
+///
+/// * **msg** is an object of type [`CheckBalancesMsg`]
+pub fn execute_check_balances(
+    ctx: &ContractContext,
+    state: &mut MPC1155ContractState,
+    msg: CheckBalancesMsg,
+) -> Vec<EventGroup> {
+    state.check_balances(msg.owner, msg.token_ids, msg.amounts);
+    vec![]
+}
