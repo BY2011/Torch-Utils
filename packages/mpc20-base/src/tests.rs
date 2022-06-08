d31970b8b1fedcd52eb138e4496d7dc982903b17
BY2011 <96490111+BY2011@users.noreply.github.com>
2022-06-08 15:08:01 +0000

    regenerate html

--- packages/mpc20-base/src/tests.rs
+++ packages/mpc20-base/src/tests.rs
@@ -272,4 +272,10 @@ fn zero_amount_on_mint() {
 #[test]
 #[should_panic(expected = "Minting is disabled")]
 fn minting_is_disabled_on_mint() {
-    let msg = Mpc20I
+    let msg = Mpc20InitMsg {
+        info: mock_token_info(),
+        initial_balances: vec![],
+        minter: None,
+    };
+
+    let (mut state, _) = execute_init(&mock_contract_context(2u8
