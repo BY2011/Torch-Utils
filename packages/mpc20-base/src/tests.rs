0ae749500025d38ca2670659bea388cdf0f35aa3
BY2011 <96490111+BY2011@users.noreply.github.com>
2022-06-25 19:27:16 +0000

    TTY: Fix tty miss restart after we turn off flow-control

--- packages/mpc20-base/src/tests.rs
+++ packages/mpc20-base/src/tests.rs
@@ -378,4 +378,12 @@ fn transfer_zero_amount() {
 
     let msg = Mpc20InitMsg {
         info: mock_token_info(),
-        initial_balances: v
+        initial_balances: vec![],
+        minter: None,
+    };
+
+    let (mut state, _) = execute_init(&mock_contract_context(2u8), &msg);
+
+    let transfer_msg = TransferMsg {
+        to: mock_address(bob),
+        amount:
