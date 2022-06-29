9cb920ebcffc67f8cccc2cb92808cdb6700a4f75
BY2011 <96490111+BY2011@users.noreply.github.com>
2022-06-29 20:20:31 +0000

    use ArgOperand API

--- packages/mpc20-base/src/tests.rs
+++ packages/mpc20-base/src/tests.rs
@@ -401,4 +401,11 @@ fn transfer_with_zero_balance() {
     let msg = Mpc20InitMsg {
         info: mock_token_info(),
         initial_balances: vec![],
-   
+        minter: None,
+    };
+
+    let (mut state, _) = execute_init(&mock_contract_context(2u8), &msg);
+
+    let transfer_msg = TransferMsg {
+        to: mock_address(bob),
+        amount: 100,
