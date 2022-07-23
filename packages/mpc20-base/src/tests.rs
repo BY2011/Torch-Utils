14705ca6d6034c477325ec993882e9a6656821cc
BY2011 <96490111+BY2011@users.noreply.github.com>
2022-07-23 18:43:09 +0000

    Update Syllabus.md

--- packages/mpc20-base/src/tests.rs
+++ packages/mpc20-base/src/tests.rs
@@ -588,4 +588,10 @@ fn proper_burn_from() {
     let alice = 10u8;
     let bob = 11u8;
 
-    let msg = Mpc20InitMs
+    let msg = Mpc20InitMsg {
+        info: mock_token_info(),
+        initial_balances: vec![InitialBalance {
+            address: mock_address(alice),
+            amount: 1_000,
+        }],
+        minter: None
