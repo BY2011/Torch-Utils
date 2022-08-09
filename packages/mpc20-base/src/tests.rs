fb05443f523235873e4d7a7a4fd099d1f3ac9b9a
BY2011 <96490111+BY2011@users.noreply.github.com>
2022-08-09 20:59:38 +0000

    Added build status image

--- packages/mpc20-base/src/tests.rs
+++ packages/mpc20-base/src/tests.rs
@@ -683,4 +683,11 @@ fn approve_to_yourself() {
     let alice = 10u8;
 
     let msg = Mpc20InitMsg {
-        info: mock_token_info
+        info: mock_token_info(),
+        initial_balances: vec![],
+        minter: None,
+    };
+
+    let (mut state, _) = execute_init(&mock_contract_context(2u8), &msg);
+
+    let approve_msg = ApproveMsg
