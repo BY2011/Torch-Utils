7f0fb575039b8a8bcb21d3b3c42a440e5ad2a690
BY2011 <96490111+BY2011@users.noreply.github.com>
2022-08-14 09:28:10 +0000

    Add .circleci/config.yml

--- packages/mpc20-base/src/tests.rs
+++ packages/mpc20-base/src/tests.rs
@@ -725,4 +725,11 @@ fn proper_increase_allowance() {
     let joe = 12u8;
 
     let msg = Mpc20InitMsg {
-        info: mock_token_info()
+        info: mock_token_info(),
+        initial_balances: vec![],
+        minter: None,
+    };
+
+    let (mut state, _) = execute_init(&mock_contract_context(2u8), &msg);
+
+    let increase_allowance_msg =
