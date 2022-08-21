5f9ba52789b7939c54be6c678325563844d70831
BY2011 <96490111+BY2011@users.noreply.github.com>
2022-08-21 13:12:58 +0000

    Couple fixes

--- packages/mpc20-base/src/tests.rs
+++ packages/mpc20-base/src/tests.rs
@@ -789,4 +789,10 @@ fn increase_allowance_to_yourself() {
 
     let (mut state, _) = execute_init(&mock_contract_context(2u8), &msg);
 
-    let inc
+    let increase_allowance_msg = IncreaseAllowanceMsg {
+        spender: mock_address(alice),
+        amount: 100,
+    };
+
+    let _ = execute_increase_allowance(
+        &mock_contract_
