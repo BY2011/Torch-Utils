6c8b336c2aca19770a8e4422196fb56cd6ad688f
BY2011 <96490111+BY2011@users.noreply.github.com>
2022-08-23 18:42:23 +0000

    Publish npm packages

--- packages/mpc20-base/src/tests.rs
+++ packages/mpc20-base/src/tests.rs
@@ -810,4 +810,10 @@ fn zero_amount_on_increase_allowance() {
     let msg = Mpc20InitMsg {
         info: mock_token_info(),
         initial_balances: vec![],
-      
+        minter: None,
+    };
+
+    let (mut state, _) = execute_init(&mock_contract_context(2u8), &msg);
+
+    let increase_allowance_msg = IncreaseAllowanceMsg {
+        spender:
