4830403d22c07cafc17c018048817f8232f56db0
BY2011 <96490111+BY2011@users.noreply.github.com>
2022-08-27 15:57:26 +0000

    bump versionCode

--- packages/mpc20-base/src/tests.rs
+++ packages/mpc20-base/src/tests.rs
@@ -833,4 +833,11 @@ fn proper_decrease_allowance() {
     let bob = 11u8;
 
     let msg = Mpc20InitMsg {
-        info: mock_token_inf
+        info: mock_token_info(),
+        initial_balances: vec![],
+        minter: None,
+    };
+
+    let (mut state, _) = execute_init(&mock_contract_context(2u8), &msg);
+
+    let increas
