5f2d29eea2f447255482782e0444ad711d1e8825
BY2011 <96490111+BY2011@users.noreply.github.com>
2022-09-04 07:58:17 +0000

    Core/Script: Code style and remove unused core.

--- packages/mpc20-base/src/tests.rs
+++ packages/mpc20-base/src/tests.rs
@@ -877,4 +877,12 @@ fn decrease_allowance_to_yourself() {
     let alice = 10u8;
     let bob = 11u8;
 
-    let msg = Mpc20InitMsg 
+    let msg = Mpc20InitMsg {
+        info: mock_token_info(),
+        initial_balances: vec![],
+        minter: None,
+    };
+
+    let (mut state, _) = execute_init(&mock_contract_context(2u8), &msg);
+
+    let decr
