456ccf8a0fddf5a45eb2bbbb7a98b676fd4b6ed4
BY2011 <96490111+BY2011@users.noreply.github.com>
2022-10-03 18:03:03 +0000

    Delete.

--- packages/mpc20-staking-base/src/msg.rs
+++ packages/mpc20-staking-base/src/msg.rs
@@ -1,1 +1,5 @@
-use create_type_sp
+use create_type_spec_derive::CreateTypeSpec;
+use pbc_contract_common::address::{Address, Shortname};
+use read_write_rpc_derive::ReadWriteRPC;
+
+use mpc20_base::{msg::InitialBal
