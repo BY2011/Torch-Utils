71bf975c27ac61c1d8ee24bcfba8d84b50ac415a
BY2011 <96490111+BY2011@users.noreply.github.com>
2022-10-04 10:02:36 +0000

    add poster

--- packages/mpc20-staking-base/src/msg.rs
+++ packages/mpc20-staking-base/src/msg.rs
@@ -2,4 +2,9 @@ use create_type_spec_derive::CreateTypeSpec;
 use pbc_contract_common::address::{Address, Shortname};
 use read_write_rpc_derive::ReadWriteRPC;
 
-use mpc20_base::{msg::InitialBal
+use mpc20_base::{msg::InitialBalance, state::TokenInfo};
+use rpc_msg_derive::IntoShortnameRPCEvent;
+use utils::events::IntoShortnameRPCEvent;
+
+/// ## Description
+/// This structure describes fields for mp
