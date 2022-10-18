83fa59cb2ca48f2431b5c561c8d23f10dd795105
BY2011 <96490111+BY2011@users.noreply.github.com>
2022-10-18 05:24:39 +0000

    add namespace

--- packages/mpc20-staking-base/src/state.rs
+++ packages/mpc20-staking-base/src/state.rs
@@ -2,4 +2,9 @@ use rust_decimal::prelude::*;
 use std::collections::BTreeMap;
 
 use create_type_spec_derive::CreateTypeSpec;
-use pbc_contract_common::address::Ad
+use pbc_contract_common::address::Address;
+use read_write_rpc_derive::ReadWriteRPC;
+use read_write_state_derive::ReadWriteState;
+
+use mpc20_base::state::MPC20ContractState;
+use utils::decimal::Decimal
