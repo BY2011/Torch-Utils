7c6167cd64c6eb05223c51a09bd24963d99b1674
BY2011 <96490111+BY2011@users.noreply.github.com>
2022-12-02 10:59:53 +0000

    Paper

--- packages/mpc721-base/src/state.rs
+++ packages/mpc721-base/src/state.rs
@@ -9,3 +9,162 @@ use read_write_state_derive::ReadWriteState;
 use crate::ContractError;
 
 /// ## Description
+/// This structure describes main mpc721 contract state.
+#[derive(ReadWriteState, CreateTypeSpec, Clone, PartialEq, Eq, Debug)]
+pub struct MPC721ContractState {
+    /// optional owner address
+    pub owner: Option<Address>,
+    /// token name
+    pub name: String,
+    /// token symbol
+    pub symbol: String,
+    /// optional base uri
+    pub base_uri: Option<String>,
+    /// minter address
+    pub minter: Address,
+    /// current supply
+    pub supply: u128,
+    /// token info by token id
+    pub tokens: BTreeMap<u128, TokenInfo>,
+    /// token approvals
+    pub operator_approvals: BTreeMap<Address, BTreeMap<Address, bool>>,
+}
+
+/// ## Description
+/// This structure describes minted mpc721 token information
+#[derive(ReadWriteRPC, ReadWriteState, CreateTypeSpec, Clone, PartialEq, Eq, Debug)]
+pub struct TokenInfo {
+    /// token owner
+    pub owner: Address,
+    /// token approvals
+    pub approvals: Vec<Address>,
+    /// optional token uri
+    pub token_uri: Option<String>,
+}
+
+impl MPC721ContractState {
+    /// ## Description
+    /// Sets new base uri
+    /// ## Params
+    /// * **base_uri** is an object of type [`str`]
+    pub fn set_base_uri(&mut self, base_uri: &str) {
+        self.base_uri = Some(base_uri.to_string())
+    }
+
+    /// ## Description
+    /// Mints new token id to specified address
+    /// ## Params
+    /// * **token_id** is a field of type [`u128`]
+    ///
+    /// * **to** is an object of type [`Address`]
+    ///
+    /// * **token_uri** is an object of type [`Option<String>`]
+    pub fn mint(&mut self, token_id: u128, to: &Address, token_uri: &Option<String>) {
+        let token = TokenInfo {
+            owner: *to,
+            approvals: vec![],
+            token_uri: token_uri.clone(),
+        };
+
+        self.tokens.insert(token_id, token);
+    }
+
+    /// ## Description
+    /// Increases total supply
+    pub fn increase_supply(&mut self) {
+        self.supply = self.supply.checked_add(1).unwrap()
+    }
+
+    /// ## Description
+    /// Decreases total supply
+    pub fn decrease_supply(&mut self) {
+        self.supply = self.supply.checked_sub(1).unwrap()
+    }
+
+    /// ## Description
+    /// Transfers specified token id to the new owner
+    /// ## Params
+    /// * **from** is an object of type [`Address`]
+    ///
+    /// * **to** is an object of type [`Address`]
+    ///
+    /// * **token_id** is an object of type [`u128`]
+    pub fn transfer(&mut self, from: &Address, to: &Address, token_id: u128) {
+        let token = self.tokens.get(&token_id).unwrap();
+        assert!(
+            Self::allowed_to_transfer(from, token, &self.operator_approvals),
+            "{}",
+            ContractError::Unauthorized
+        );
+
+        self.tokens.entry(token_id).and_modify(|t| {
+            t.owner = *to;
+            t.approvals = vec![];
+        });
+    }
+
+    /// ## Description
+    /// Updates token approvals
+    /// ## Params
+    /// * **from** is an object of type [`Address`]
+    ///
+    /// * **spender** is an object of type [`Address`]
+    ///
+    /// * **token_id** is an object of type [`u128`]
+    ///
+    /// * **approved** is an object of type [`bool`]
+    pub fn update_approvals(
+        &mut self,
+        from: &Address,
+        spender: &Address,
+        token_id: u128,
+        approved: bool,
+    ) {
+        let token = self.tokens.get(&token_id).unwrap().to_owned();
+        assert!(
+            Self::allowed_to_approve(from, &token, &self.operator_approvals),
+            "{}",
+            ContractError::Unauthorized,
+        );
+
+        let mut approvals = token
+            .approvals
+            .into_iter()
+            .filter(|account| account != spender)
+            .collect::<Vec<Address>>();
+
+        if approved {
+            approvals.push(*spender);
+        }
+
+        self.tokens
+            .entry(token_id)
+            .and_modify(|t| t.approvals = approvals);
+    }
+
+    /// ## Description
+    /// Adds operator approval
+    /// ## Params
+    /// * **owner** is an object of type [`Address`]
+    ///
+    /// * **operator** is an object of type [`Address`]
+    pub fn add_operator(&mut self, owner: &Address, operator: &Address) {
+        let owner_operators = self
+            .operator_approvals
+            .entry(*owner)
+            .or_insert_with(BTreeMap::new);
+
+        owner_operators.insert(*operator, true);
+    }
+
+    /// ## Description
+    /// Removes operator approval
+    /// ## Params
+    /// * **owner** is an object of type [`Address`]
+    ///
+    /// * **operator** is an object of type [`Address`]
+    pub fn remove_operator(&mut self, owner: &Address, operator: &Address) {
+        let owner_operators = self
+            .operator_approvals
+            .get_mut(owner)
+            .unwrap_or_else(|| panic!("{}", ContractError::NotFound.to_string()));
