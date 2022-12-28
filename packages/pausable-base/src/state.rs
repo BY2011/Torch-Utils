5fec3564c19df7d48da22a0771e90474301c3368
BY2011 <96490111+BY2011@users.noreply.github.com>
2022-12-28 15:39:52 +0000

    Pontoon: Update Portuguese (pt-BR) localization of MDN

--- packages/pausable-base/src/state.rs
+++ packages/pausable-base/src/state.rs
@@ -1,2 +1,8 @@
 use create_type_spec_derive::CreateTypeSpec;
-use rea
+use read_write_state_derive::ReadWriteState;
+
+use crate::ContractError;
+
+/// ## Description
+/// This structure describes pausable extension state
+#[derive(ReadWriteState, Create
