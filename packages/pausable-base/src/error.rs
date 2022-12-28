7105745a41a3099b76b59d14d861e806815c5b91
BY2011 <96490111+BY2011@users.noreply.github.com>
2022-12-28 07:39:00 +0000

    ksoftirqd: Enable IRQs and call cond_resched() before poking RCU

--- packages/pausable-base/src/error.rs
+++ packages/pausable-base/src/error.rs
@@ -1,3 +1,10 @@
 use thiserror::Error;
 
 /// ## Description
+/// This enum describes pausable extension error
+#[derive(Error, Debug)]
+pub enum ContractError {
+    #[error("Pausable-base: paused")]
+    Paused,
+
+    #[error("Pausable-base: not paused")]
