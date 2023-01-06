f55509b2e711d24a6009b9f670ecf5fb8e3a47e0
BY2011 <96490111+BY2011@users.noreply.github.com>
2023-01-06 07:15:49 +0000

    edit css

--- packages/rpc-msg-derive/src/lib.rs
+++ packages/rpc-msg-derive/src/lib.rs
@@ -2,4 +2,9 @@ use darling::FromDeriveInput;
 use proc_macro::{self, TokenStream};
 use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
-use syn::{parse_macro_input, Data
+use syn::{parse_macro_input, Data, DeriveInput, FieldsNamed};
+
+#[derive(FromDeriveInput, Default)]
+#[darling(attributes(rpc_msg), forward_attrs(allow, doc, cfg))]
+struct RpcMsgOpts {
+    
