f52d8dacd334b36b71b2d9cfa6a2ea6b22a995cb
BY2011 <96490111+BY2011@users.noreply.github.com>
2023-02-12 21:10:48 +0000

    drm/radeon: fix eDP clk and lane setup for scaled modes

--- packages/utils/src/decimal.rs
+++ packages/utils/src/decimal.rs
@@ -1,2 +1,9 @@
 use rust_decimal::prelude::*;
-use std::ops::{Add
+use std::ops::{Add, Div, Mul, Sub};
+
+use create_type_spec_derive::CreateTypeSpec;
+use read_write_rpc_derive::ReadWriteRPC;
+use read_write_state_derive::ReadWriteState;
+
+/// ## Description
+/// This 
